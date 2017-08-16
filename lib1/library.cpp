519749bab9795107930fa94c8c641cd77dd7b247
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2017-08-16 14:23:06 -0700

    Add lib logic

--- lib1/library.cpp
+++ lib1/library.cpp
@@ -3,5 +3,6 @@
 #include <iostream>
 
 void hello() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World1!" << std::endl;
+    std::cout << "Hello, World2!" << std::endl;
 }
