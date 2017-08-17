bb1303168044d77cf6c9dcd9c80990dc4ce96fd2
Gerrit <g.h.vandoorn@gmail.com>
2017-08-17 10:45:42 -0700

    Update library.cpp

--- lib1/library.cpp
+++ lib1/library.cpp
@@ -6,4 +6,5 @@ void hello() {
     std::cout << "Hello, World1!" << std::endl;
     std::cout << "Hello, World2!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
+    std::cout << "Hello, World4!" << std::endl;
 }
